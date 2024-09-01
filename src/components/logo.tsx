5ce289925a3dd95cd8514739da5137bb2d0fac59
Idris Elmi <idriselmi0@gmail.com>
2024-09-01 21:37:44 +0200

    optimized logo image

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Logo() {
-    return (
-        <Link href="/">
-            <div className="flex items-center">
-                <img src="/logo.svg" alt="Logo" className="w-40 h-16" />
-            </div>
-        </Link>
-    );
+  return (
+    <Link href="/">
+      <div className="flex items-center">
+        <Image src="/logo.svg" alt="Logo" className="w-40 h-16" />
+      </div>
+    </Link>
+  );
 }
