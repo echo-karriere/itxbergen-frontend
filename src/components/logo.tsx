7bc7f163c99e5ab4b8c685331e8ca7f95b14f6c6
Idris Elmi <idriselmi0@gmail.com>
2024-09-01 22:16:47 +0200

    fixed image

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
 import Image from "next/image";
+import React from "react";
 
 export default function Logo() {
   return (
     <Link href="/">
       <div className="flex items-center">
-        <Image src="/logo.svg" alt="Logo" className="w-40 h-16" />
+        <Image
+          src="/logo.svg"
+          alt="Logo"
+          width={160}
+          height={60}
+          className="w-40 h-16" />
       </div>
     </Link>
   );
