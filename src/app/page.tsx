377f44aba9a1014c5f6750676d4a244b7c328756
Sebastian Helgesen <sebhelg@gmail.com>
2024-09-06 00:47:03 +0200

    Added portabletext/react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ export default async function Home() {
   return (
     <>
     <h1>{data[0].title}</h1>
-    
 
     </>
   );
--- src/lib/sanity.queries.ts
+++ src/lib/sanity.queries.ts
@@ -1,7 +1,6 @@
 import { groq } from "next-sanity";
 import { client } from "./sanity.client";
 
-
 export async function getPrivacy() {
   return client.fetch(
     groq`*[_type == "privacy"]{
