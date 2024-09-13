cf50fc5f12c0bd21408f440a5fc5ce3f11c7b97f
isak79 <isak.hansen74@hotmail.no>
2024-09-13 17:34:46 +0200

    fått inn noe

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import initTranslations from "../i18n.js";
 
-export default function Home() {
-  return (
-    <p>hello new website!</p>
-  );
+export default async function Home({ params: { locale } }) {
+  const { t } = await initTranslations(locale, ["default"]);
+
+  return <p>{t("test")}</p>;
 }
