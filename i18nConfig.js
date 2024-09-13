7a5c9a0c31f74ae158ae32a2d7f9f1110f41172d
isak79 <isak.hansen74@hotmail.no>
2024-09-13 12:03:40 +0200

    går alltid inn på norsk

--- i18nConfig.js
+++ i18nConfig.js
@@ -1,6 +1,8 @@
 const i18nConfig = {
-    locales: ['no','en'],
-    defaultLocale: 'no'
+  locales: ["no", "en"],
+  defaultLocale: "no",
+  localeDetection: false,
 };
 
 module.exports = i18nConfig;
+
