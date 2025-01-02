0d767020c47c4922f0c85c53c7ae56f2b9f7389b
Idris Elmi <idriselmi0@gmail.com>
2025-01-02 22:15:49 +0100

    added new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,6 +23,8 @@ const config = {
         IXBRed2: "#480413",
         IXBGold: "#d0b46c",
         GrayText: "#D3D3D3",
+        IXBbg: "#000E1E",
+        IXBfg: "#9CEDE9",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
