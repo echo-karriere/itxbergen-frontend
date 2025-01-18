5eeef021b28245b7d0ddaa3b1a60308e55217e19
Idris Elmi <idriselmi0@gmail.com>
2025-01-18 15:19:27 +0100

    updated positioning of elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,9 +16,9 @@ export default function Home() {
   return (
     <div>
       {/* Section 1 */}
-      <div className="bg-IXBbg2 text-white h-[100vh] flex items-center relative">
+      <div className="bg-IXBbg2 text-white h-[95vh] flex items-center relative">
         {/* Left Side Content */}
-        <div className="flex flex-col justify-center items-start pl-40 space-y-16 w-[60%] mb-64">
+        <div className="flex flex-col justify-center items-start pl-40 space-y-16 w-[60%] mb-80">
           {/* Large Text */}
           <h1 className="text-black text-5xl font-bold">
             <span className="text-IXBPurple">Karrieredag</span> og
@@ -33,14 +33,14 @@ export default function Home() {
           />
 
           {/* Subtext */}
-          <p className="text-black">
-            Ikke karrieredag enda?
+          <p className="text-black text-2xl">
+            Ikke karrieredag enda? <br />
             Sjekk ut mulighetene for nettverking!
           </p>
         </div>
 
         {/* Right Side Image */}
-        <div className="absolute right-0 flex justify-center items-center mb-60 pr-10 w-[40%] mt-10">
+        <div className="absolute right-0 flex justify-center items-center mb-60 pr-10 w-[40%]">
           <Image
             src="/tech-company-amico.png"
             alt="ItxBergen"
