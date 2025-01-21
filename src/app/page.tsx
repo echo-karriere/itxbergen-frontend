1a8485b723e204008e0171066f7ae230b62c458f
Idris Elmi <idriselmi0@gmail.com>
2025-01-21 12:11:23 +0100

    more work on section 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,11 +15,11 @@ export default function Home() {
   return (
     <div>
       {/* Section 1 */}
-      <div className="bg-IXBbg2 text-white h-[70vh] flex flex-col md:flex-row items-center relative">
+      <div className="bg-IXBbg2 text-black h-[70vh] flex flex-col md:flex-row items-center relative">
         {/* Left Side Content */}
         <div className="flex flex-col justify-center items-start pl-40 space-y-16 w-[60%] mb-32">
           {/* Large Text */}
-          <h1 className="text-black text-3xl md:text-5xl font-bold">
+          <h1 className="text-3xl md:text-5xl font-bold">
             <span className="text-IXBPurple">Karrieredag</span> og
             <span className="text-IXBPurple"> nettverking</span> <br /> for IT-studenter i Bergen!
           </h1>
@@ -32,7 +32,7 @@ export default function Home() {
           />
 
           {/* Subtext */}
-          <p className="text-black text-base md:text-xl">
+          <p className="text-base md:text-xl">
             Ikke karrieredag enda? <br />
             Sjekk ut mulighetene for <span className="font-bold underline">nettverking</span>!
           </p>
@@ -52,12 +52,54 @@ export default function Home() {
       </div>
 
       {/* Section 2 */}
-      <div className="bg-IXBbg text-white h-[40vh] flex justify-center items-center relative">
+      <div className="bg-IXBbg text-white h-[70vh] flex flex-col justify-center items-center relative">
         {/* Wave separator at the top */}
         <div className="absolute top-0 w-full" style={{ marginTop: '-10vh' }}>
           <Image
             src="/wave_top.png"
-            alt="Wave"
+            alt="Wave Top"
+            layout="intrinsic"
+            width={1920}
+            height={1080}
+            className="w-full h-auto object-contain"
+            draggable="false"
+          />
+        </div>
+
+        {/* Content Section */}
+        <div className="w-full flex justify-between items-center px-10" style={{ zIndex: '20' }}>
+          {/* Text */}
+          <div className="w-1/2 flex flex-col space-y-16 ml-24 mb-10">
+            <h1 className="text-IXBfg text-3xl font-bold">Hva er ITxBergen?</h1>
+            <p className="text-lg md:text-xl">
+              ITxBergen arrangerer karrieredager for IT-studenter i
+              bergensområdet, og er tidligere kjent som echo karriere.
+              Arrangementet ble holdt for første gang i 2019, noe som ble en
+              stor suksess med stort oppmøte fra  IT-studenter fra hele Bergen.
+              Enkelte ganger holder vi også andre karrierefremmende
+              arrangementer.
+              <br /> <br />
+              Vi er en frivillig  studentorganisasjon som
+              samarbeider med andre linjeforeninger innenfor  IT i Bergen, for
+              å skape en felles årlig karrieredag. Vi når ut til
+              linjeforeningene ved Universitetet i Bergen, Høgskulen på
+              Vestlandet og  Høyskolen Kristiania. Samtidig skaper vi et
+              arrangement som er åpent for alle IT-studenter i Bergen, og alle
+              som er interesserte.
+            </p>
+          </div>
+
+          {/* Image */}
+          <div className="w-1/2" style={{ zIndex: '20' }}>
+
+          </div>
+        </div>
+
+        {/* Wave separator at the bottom */}
+        <div className="absolute bottom-0 w-full" style={{ marginBottom: '-10vh', zIndex: '10' }}>
+          <Image
+            src="/wave_bottom.png"
+            alt="Wave Bottom"
             layout="intrinsic"
             width={1920}
             height={1080}
@@ -69,18 +111,6 @@ export default function Home() {
 
       {/* Section 3 */}
       <div className="bg-IXBbg1 text-white h-[90vh] flex justify-center items-center relative">
-        {/* Wave separator at the bottom */}
-        <div className="absolute top-0 w-full" style={{ marginTop: '-10vh' }}>
-          <Image
-            src="/wave_bottom.png"
-            alt="Wave"
-            layout="intrinsic"
-            width={1920}
-            height={1080}
-            className="w-full h-auto object-contain"
-            draggable="false"
-          />
-        </div>
       </div>
     </div>
   );
