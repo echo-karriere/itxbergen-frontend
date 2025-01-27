56edfa91d2bed1f1c7e5402229ac75a963f8d263
isak79 <isak.hansen74@hotmail.no>
2025-01-27 13:27:28 +0100

    made a breadcrumb component(needs change if on nested pages), implemented it on the omitx page

--- src/app/om-itx/page.tsx
+++ src/app/om-itx/page.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
+import Breadcrumbs from "@/components/utils/breadcrumbs";
 
 const Page = () => {
   return (
     <>
-      <div className="flex items-start space-x-12 space-y-12 pt-10">
+      <div className="flex pl-12 space-y-12 pt-10">
         <div className="w-[37%] pl-16">
-          <p className="font-inter text-xs text-[#121212] mb-2">
-            <Link href="/">Hjem - Om ITxBergen</Link>
-          </p>
-          <h1 className="font-promptBold font-bold text-5xl leading-tight mb-6 pt-5">
+          <Breadcrumbs current={"Om ITxBergen"} />
+          <h1 className="font-promptBold font-bold text-5xl/10 leading-tight mb-6 pt-5">
             Om ITxBergen
           </h1>
-          <p className="font-sourceSans text-xs/8 leading-relaxed text-[#121212]">
+          <p className="font-sourceSans text-sm/8 text-[#121212]">
             ITxBergen arrangerer karrieredager for IT-studenter i
             bergensområdet, og er tidligere kjent som echo karriere.
             Arrangementet ble holdt for første gang i 2019, noe som ble en stor
             suksess med stort oppmøte fra IT-studenter fra hele Bergen. Enkelte
-            ganger holder vi også andre karrierefremmende arrangementer. Vi er
-            en frivillig studentorganisasjon som samarbeider med andre
+            ganger holder vi også andre karrierefremmende arrangementer.
+            <br></br>
+            <br></br>
+            Vi er en frivillig studentorganisasjon som samarbeider med andre
             linjeforeninger innenfor IT i Bergen, for å skape en felles årlig
             karrieredag. Vi når ut til linjeforeningene på Universitetet i
             Bergen, Høgskulen på Vestlandet og Høyskolen Kristiania. Samtidig
@@ -27,7 +27,7 @@ const Page = () => {
             Bergen, og alle som er interesserte.
           </p>
         </div>
-        <div className="w-[40%] flex justify-end ml-8">
+        <div className="w-[40%]">
           <Image
             src="/omitxbilde.png"
             alt="ITxBergen illustration"
---
+++ src/components/utils/breadcrumbs.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import React from "react";
+
+const Breadcrumbs = ({ current }) => {
+  return (
+    <>
+      <nav aria-label="breadcrumbs">
+        <ol className="inline-flex items-center space-x-1 font-inter text-xs text-[#121212] mb-2 font-normal">
+          <li>
+            <Link href="/">Hjem</Link>
+          </li>
+          <li>-</li>
+          <li>
+            <span className="font-bold">{current}</span>
+          </li>
+        </ol>
+      </nav>
+    </>
+  );
+};
+
+export default Breadcrumbs;
