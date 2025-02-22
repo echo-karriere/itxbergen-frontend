7e61972ecde4ba6934ca834159487df5d46390aa
Idris Elmi <idriselmi0@gmail.com>
2025-02-22 16:52:48 +0100

    update footer on this branch

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,88 +1,65 @@
 import Logo from "./logo";
 import Partners from "./partners";
-import WavPattern from "./wave-pattern";
+import WavePattern from "./wave-pattern";
 
 export default function Footer() {
   return (
     <>
-      {/* Wave Image */}
+      {/* SVG wave pattern*/}
       <div className="relative w-full -mb-[80px]">
-        <WavPattern />
+        <WavePattern />
       </div>
 
-      <footer className="relative bg-IXBbg text-white">
-        {/* Footer Content Container */}
-        <div className="relative container mx-auto py-4">
-          {/* Logo */}
-          <div className="flex justify-center md:justify-start mb-4">
-            <Logo />
+      <footer className="relative w-full h-96 bg-IXBbg flex items-center justify-center">
+        {/* Logo */}
+        <div className="absolute top-2 left-8">
+          <Logo />
+        </div>
+
+        {/* Links */}
+        <div className="w-[75%] grid grid-cols-4 gap-8 text-white text-sm text-left mb-16">
+          <div>
+            {/* Informasjon */}
+            <h3 className="font-semibold mb-4">Informasjon</h3>
+            <p className="mb-4">Karrieredag</p>
+            <p className="mb-4">Arrangementer</p>
+            <p className="mb-4">Stillingsannonser</p>
+            <p className="mb-4">Om ItxBergen</p>
           </div>
 
-          {/* Top Section: Links */}
-          <div className="flex flex-col items-center">
-            <div className="grid grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-16 text-sm max-w-screen-lg w-full mb-2">
-              {/* Informasjon */}
-              <div>
-                <h3 className="font-bold mb-4">Informasjon</h3>
-                <ul>
-                  <li className="mb-4">Karrieredag</li>
-                  <li className="mb-4">Arrangementer</li>
-                  <li className="mb-4">Stillingsannonser</li>
-                  <li className="mb-4">Om ItxBergen</li>
-                </ul>
-              </div>
-
-              {/* Kontakt oss */}
-              <div>
-                <h3 className="font-bold mb-4">Kontakt oss</h3>
-                <ul>
-                  <li className="mb-4">Kontakt oss</li>
-                  <li className="mb-4">Presse</li>
-                  <li className="mb-4">Opplevd noe ugreit?</li>
-                </ul>
-              </div>
-
-              {/* Om nettstedet */}
-              <div>
-                <h3 className="font-bold mb-4">Om nettstedet</h3>
-                <ul>
-                  <li className="mb-4">Personvernserklæring</li>
-                  <li className="mb-4">Salgs- og påmeldingsvilkår</li>
-                  <li className="mb-4">Tilgjengelighet</li>
-                  <li className="mb-4">Informasjonskapsler</li>
-                </ul>
-              </div>
-
-              {/* Sosiale medier */}
-              <div>
-                <h3 className="font-bold mb-4">Sosiale medier</h3>
-                <ul>
-                  <li className="mb-4">LinkedIn</li>
-                  <li className="mb-4">Instagram</li>
-                  <li className="mb-4">Facebook</li>
-                  <li className="mb-4">GitHub</li>
-                </ul>
-              </div>
-            </div>
+          {/* Kontakt oss */}
+          <div>
+            <h3 className="font-semibold mb-4">Kontakt oss</h3>
+            <p className="mb-4">Kontakt oss</p>
+            <p className="mb-4">Presse</p>
+            <p className="mb-4">Opplevd noe ugreit?</p>
           </div>
 
-          {/* text */}
-          <div className="flex justify-center text-sm font-bold mb-2 mt-4">
-            <p>ITxBergen © 2019 - 2024</p>
+          {/* Om nettstedet */}
+          <div>
+            <h3 className="font-semibold mb-4">Om nettstedet</h3>
+            <p className="mb-4">Personvernserklæring</p>
+            <p className="mb-4">Salgs- og påmeldingsvilkår</p>
+            <p className="mb-4">Tilgjengilighet</p>
+            <p className="mb-4">Informasjonskapsler</p>
           </div>
 
-          {/* Horizontal Line */}
-          <div className="w-full border-t border-white mb-2"></div>
-
-          {/* "Powered by" Text */}
-          <div className="flex justify-center text-sm mb-2 mt-4">
-            <p>Powered by:</p>
+          {/* Sosiale medier */}
+          <div>
+            <h3 className="font-semibold mb-4">Sosiale medier</h3>
+            <p className="mb-4">Linkedin</p>
+            <p className="mb-4">Instagram</p>
+            <p className="mb-4">Facebook</p>
+            <p className="mb-4">Github</p>
           </div>
+        </div>
 
-          {/* Bottom Section: Partners */}
-          <div className="flex justify-center mb-2">
-            <Partners />
-          </div>
+        {/* Horizontal Line */}
+        <div className="absolute bottom-[25%] left-1/2 -translate-x-1/2 w-[75%] border-t border-white" />
+
+        {/* Parters */}
+        <div className="absolute bottom-8 left-1/2 -translate-x-1/2">
+          <Partners />
         </div>
       </footer>
     </>
