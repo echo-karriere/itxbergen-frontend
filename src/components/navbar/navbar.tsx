f6b46461a4db81b5b705d00bebcaaea6d8413d30
lockels <123327897+lockels@users.noreply.github.com>
2025-02-24 14:48:32 +0100

    updates to navbar (#52)

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,28 +1,32 @@
 import Logo from "./logo";
 import LanguagePicker from "./language-picker";
+import Link from "next/link";
 
 export default function Navbar() {
+
   return (
     <>
       <div className="w-full h-20 bg-IXBbg top-0 flex items-center">
         <div className="container mx-auto px-4 h-full">
-          <div className="flex items-center h-full">
+          <div className="flex items-center justify-between h-full">
             {/* Logo Container */}
-            <div className="absolute left-3 pl-6">
+            <div className="pl-6">
               <Logo />
             </div>
-            {/* Spacer */}
+
+            {/* Centered Navigation */}
             <div className="flex-grow flex justify-center">
-              <ul className="hidden md:flex gap-x-20 text-white items-center text-sm">
-                <li>Karrieredag </li>
-                <li>Nettverking </li>
+              <ul className="hidden md:flex gap-x-6 md:gap-x-20 text-white items-center text-sm">
+                <Link href="/karrieredag">Karrieredag </Link>
+                <Link href="/nettverking">Nettverking </Link>
                 <li>Stillingsannonser </li>
                 <li>Om ITxBergen </li>
                 <li>Kontakt oss</li>
               </ul>
             </div>
+
             {/* Right Side Buttons */}
-            <div className="absolute right-16">
+            <div className="pr-6">
               <LanguagePicker />
             </div>
           </div>
