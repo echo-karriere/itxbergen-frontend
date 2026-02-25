3db3d888e3df3336fa726dc1878b729b3ed049b9
Isak Hansen <isakhansen@hackerman.local>
2026-02-25 18:23:41 +0100

    vilkar

---
+++ src/app/vilkar/page.tsx
@@ -0,0 +1,121 @@
+
+export default function Page() {
+	return (
+		<div className="w-full bg-white py-12">
+			<div className="maxwidth text-black flex flex-col md:flex-row px-6 md:px-32 space-y-12 md:space-y-0 md:space-x-20">
+				<div className="flex flex-col justify-center md:w-3/5 space-y-4">
+					<h1 className="text-2xl font-bold">
+						Avtale- og påmeldingsvilkår for karrieredagen til ITxBERGEN 2026
+					</h1>
+					Gjelder fra: 23.02.2026 kl. 00:00
+					Gjelder til: Inntil videre
+
+					<span className="text-2xl text-[#21465F] pt-6">I. Avtalen</span>
+					<p className="mb-4 text-md">
+						Avtalen består av disse salgsbetingelsene, opplysninger gitt i bestillingsløsningen og eventuelt særskilt avtalte vilkår. Ved eventuell motstrid mellom opplysningene, går det som særskilt er avtalt mellom partene foran, så fremt det ikke strider mot ufravikelig lovgivning.
+						Avtalen vil i tillegg bli utfylt av relevante ufravikelige lovbestemmelser som regulerer kjøp av tjenester mellom næringsdrivende.
+					</p>
+
+					<span className="text-2xl text-[#21465F] pt-2">II. Partene</span>
+					<p className="pb-6">
+						Selger/Arrangør er ITXBERGEN, org.nr.: 924 504 013, adresse: Thormøhlens gate 55, 5006 BERGEN og betegnes i det følgende som arrangør/arrangøren.
+						Kjøper/Deltaker er den part som foretar påmeldingen, og betegnes i det følgende som deltaker/deltakeren.
+					</p>
+					<span className="text-2xl text-[#21465F] mt-2">III. Pris</span>
+					Alle priser er oppgitt med eksklusiv merverdiavgift. Ytterligere kostnader som deltaker før avtaleinngåelse ikke har blitt informert om via nettsiden https://prisliste.ixb.no, skal deltaker ikke bære.
+					Hvis deltaker får oppført feil prisinformasjon i bestillingsløsningen skal prisene som står oppført på nettsiden https://prisliste.ixb.no overveie.
+					<span className="text-2xl text-[#21465F] mt-2">III.a. Rabatt til startup bedrifter</span>
+					Selskaper som faller under alle disse kriteriene, vil få rabatt på grunnpakken:
+					<ol className="list-[upper-roman] list-inside ml-10 pb-2">
+						<li>Etablert etter 17. september 2020</li>
+						<li>25(tjuefem) eller færre ansatte</li>
+						<li>Driftsinntekter i 2023(totusenogtjuetre) på under 35(trettifem) MNOK</li>
+					</ol>
+					Om disse kriteriene ikke stemmer, vil arrangør kunne endre prisen til fullpris, og avtalen er fortsatt bindende. Om deltaker er usikker på om de kvalifiserer til rabatt, så skal deltaker ta kontakt med arrangør før avtaleinngåelse. Arrangør vil sjekke om kravene er oppfylt opp mot offentlig informasjon fra Brønnøysundregistrene.
+
+					<span className="text-2xl text-[#21465F]">III.b. Rabatt til statlige, fylkeskommunale eller kommunale selskaper eller organisasjoner</span>
+					<p>
+						Selskaper eller organisasjoner som faller under minst en av disse kriteriene, vil få rabatt på grunnpakken:
+					</p>
+
+					<ol className="list-[upper-roman] list-inside ml-10 pb-2">
+						<li>Statlig organisasjon, Fylkeskommune og Kommune</li>
+						<li>Statlige selskaper med 50%(femti prosent) eller høyere eierskap andel under kategori 2 (sjekk regjeringen.no)</li>
+						<li>Fylkeskommunale og eller kommunale selskaper med 50%(femti prosent) eller høyere eierskapsandel der målet er mest mulig effektiv oppnåelse av sektorpolitiske mål, og opererer primært ikke i konkurranse med andre.</li>
+					</ol>
+					<p className="mb-4">Om en av disse kriteriene ikke stemmer, vil arrangør kunne endre prisen til fullpris, og avtalen er fortsatt bindende. Om deltaker er usikker på om de kvalifiserer til rabatt, så skal deltaker ta kontakt med arrangør før avtaleinngåelse. Arrangør vil sjekke om kravene er oppfylt opp mot offentlig tilgjengelig informasjon.</p>
+					<p className="pt-2">Selskaper som faller under minst en av disse kriteriene vil ikke kvalifisere til rabatt:</p>
+					<ol className="list-[upper-roman] list-inside ml-10 pb-2">
+						<li>Statlige selskaper uansett eierskap andel under kategori 12</li>
+						<li>Fylkeskommunale og eller kommunale selskaper uansett eierskap andel som har mål om høyest mulig avkastning over tid, og opererer primært i konkurranse med andre selskaper.</li>
+						<li>Strøm og nettselskap.</li>
+					</ol>
+
+					<span className="text-2xl text-[#21465F] p">III.c. Ekstra gebyrer</span>
+					Arrangør kan pådra deltaker ekstra gebyrer etter del VI.a., VI.b. og VI.c. av denne avtalen.
+					<span className="text-2xl text-[#21465F]">IV. Avtaleinngåelse</span>
+					Avtalen er bindende for begge parter i det øyeblikk deltakeren har sendt sin påmelding til arrangør.
+
+					<span className="text-2xl text-[#21465F]">V. Arrangementet</span>
+					<p className="pb-6">
+						Arrangør er ansvarlig for arrangementet sin gjennomføring, kvalitet og innhold. Arrangør forbeholder seg retten til å endre arrangementet sitt innhold, og deltaker aksepterer at arrangementets innhold kan fravike fra beskrivelsen oppgitt til deltaker ved avtaleinngåelse.
+					</p>
+					<span className="text-2xl text-[#21465F]">VI. Deltakerens plikter</span>
+					<span className="text-2xl text-[#21465F]">VI.a. Bindende påmelding</span>
+					<p className="pb-6">
+						Som skrevet i del IV er avtalen bindende. Dersom deltakeren avbestiller etter avtaleinngåelse, vil deltaker bli fakturert det ordinære beløpet. Avbestillinger etter 1. juni vil komme med et avbestillingsgebyr på 100% (etthundre prosent), dette gjelder grunnpakken og eventuelle tilleggspakker.
+					</p>
+					<span className="text-2xl text-[#21465F]">VI.b. Oppmøtefrist og kriterier</span>
+					<p className="pb-6">
+						Dersom deltakeren ikke møter med minst to representanter på standen innen klokken 11:00 (elleve null null) vil dette medføre et gebyr på 50 % (femti prosent) av grunnpakken. Skjønn kan utøves dersom arrangør får beskjed. Om deltaker ikke møter til arrangementet i det hele tatt vil denne avgiften øke til 100%(etthundre prosent), dette gjelder grunnpakken og eventuelle tilleggspakker.
+					</p>
+					<span className="text-2xl text-[#21465F]">VI.c. Nektet adgang til arrangement</span>
+					<p className="pb-6">
+						Dersom betaling ikke er mottatt før arrangementets start, vil deltaker nektes deltakelse og et gebyr på 50% (femti prosent) vil bli lagt til på av totalbeløpet til faktura.
+					</p>
+					<span className="text-2xl text-[#21465F]">VI.d. Obligatoriske frister</span>
+					<p className="pb-6">
+						Dersom obligatoriske frister ikke overholdes, har arrangør rett til å frata konseptpresentasjon, lynpresentasjon, og/eller workshop, og gi den videre til deltakere på ventelisten. Deltakeren vil fortsatt bli belastet for konseptpresentasjonen, lynpresentasjonen, og/eller workshop. Fristene kan gis etter avtaleinngåelse.
+					</p>
+					<span className="text-2xl text-[#21465F]">VI.e. Galla</span>
+					<p className="pb-6">
+						Gallaen er bindende ved avtaleinngåelse. Deltakerens representanter forpliktes til å opptre anstendig under gallaen.
+					</p>
+					<span className="text-2xl text-[#21465F]">VI.f. Myndighet</span>
+					<p className="pb-6">
+						Kontaktpersonen som melder deltaker på dette arrangementet, bekrefter ved avtaleinngåelse at han/hun/hen/dem/etc. har myndighet til dette.
+					</p>
+					<span className="text-2xl text-[#21465F]">VI.g. Tillegg</span>
+					<p className="pb-6">
+						Alt utstyr utover det som er inkludert i pakken må fikses på eget ansvar, dette er fordi vi avholder karrieredagen i et leid lokale, og har derfor begrenset tilgang til ekstra utstyr.
+						Vi er avhengig av at deltaker overholder sin del, slik at vi sammen får til et velfungerende arrangement.
+					</p>
+					<span className="text-2xl text-[#21465F]">VII. Begrensede plasser på pakker</span>
+					<p className="pb-6">
+						Karrieredagen med sine tilhørende tilleggspakker som workshop og galla kan ha begrensede plasser. De første påmeldte deltakerne vil ha førsterett på disse plassene, og resten vil havne på venteliste. deltakere på venteliste vil ikke bli fakturert for den gjeldende pakken dersom de ikke får plass.
+					</p>
+					<span className="text-2xl text-[#21465F]">VIII. Force Majeure</span>
+					<p className="pb-6">
+						Hendelser utenfor arrangørens kontroll vil medføre at arrangøren kan velge å flytte arrangementet frem i tid. Eventuelle ekstragebyrer utenom ordinærbeløp vil frafalle om deltaker ikke har mulighet til å delta om arrangementet flyttes frem i tid grunnet en force majeure hendelse. Dette gjelder ikke ekstragebyrer som ble gitt før force majeure hendelsen inntraff.
+					</p>
+					<span className="text-2xl text-[#21465F]">IX. Endring av vilkår</span>
+					<p className="pb-6">
+						Arrangør vil ha mulighet til å kunne endre avtalevilkårene på et senere tidspunkt om nødvendig. Om deltaker meldte seg på arrangementet før vilkårene ble endret, vil den versjonen av vilkårene deltaker bekreftet være den gjeldende avtalen. Tidligere versjoner av vilkårene vil bli lenket til en ekstern nettside hvis dette blir relevant.
+					</p>
+					<span className="text-2xl text-[#21465F]">X. Kontaktinformasjon</span>
+					<p className="pb-6">
+						Arrangør kan kontaktes på e-post kontakt@itxbergen.no
+					</p>
+					<span className="text-2xl text-[#21465F]">XI.  Language Clause</span>
+					<p className="pb-6">
+						Any translation into another language shall be deemed as reference, and the Norwegian Bokmål version shall prevail in any case.
+					</p>
+					<span className="text-2xl text-[#21465F]">XII. Konfliktløsing</span>
+					<p className="pb-6">
+						Dersom det skulle utvikle seg uenigheter eller tvister om avtalen, skal partene søke å oppnå enighet i minnelighet seg imellom. Rett verneting for avtalen generelt er Hordaland Tingrett.
+					</p>
+				</div>
+			</div>
+		</div>
+	)
+}
