1702415dc3d34dd137daf129936cd94f4f2d7ff6
LazyTurtle <123999336+StefanMalv@users.noreply.github.com>
2026-08-31 11:13:16 +0200

    Små endringer til formatering og lagt til knapper som kommentarer

--- src/app/home/page-header.tsx
+++ src/app/home/page-header.tsx
@@ -6,18 +6,20 @@ export default function PageHeader() {
     <section className="w-full bg-IXBbg2 py-12">
       <div className="maxwidth flex flex-col md:flex-row px-6 md:px-32 space-y-12 md:space-y-0 md:space-x-20">
         {/* Text Content */}
-        <div className="flex flex-col justify-center md:w-3/5 space-y-8">
-          <h1 className="text-3xl md:text-4xl font-bold">
+        <div className="flex flex-col justify-center md:w-3/5 space-y-7">
+          <h1 className="text-3xl md:text-4xl font-bold leading-tight">
             <span className="text-IXBPurple">Karrieredag</span> og
             <span className="text-IXBPurple"> nettverking</span> <br /> for
             IT-studenter i Bergen!
           </h1>
 
-          <p className="text-xl md:text-xl">
-            Neste karrieredag er satt <br></br> <span className="font-bold">17. September 2026 i Grieghallen!</span>
+          <p className="-mt-3 text-lg md:text-xl">
+            <span className="font-bold text-IXBPurple">
+              17. September 2026 i Grieghallen!
+            </span>
           </p>
 
-          <div className="space-y-2 pl-3 md:pl-0">
+          <div className="space-y-3 pl-3 md:pl-0">
             <ITxButton
               label="Timeplan"
               variant="primary"
@@ -31,7 +33,13 @@ export default function PageHeader() {
               href="/nettverking"
             />
             {/* <ITxButton
-              label="Se priser for karrieredagen"
+              label="Meld deg på Workshop!"
+              variant="primary"
+              className="w-80"
+              href="/karrieredag/priser"
+            /> */}
+            {/* <ITxButton
+              label="Meld deg på AI på scenen!"
               variant="primary"
               className="w-80"
               href="/karrieredag/priser"
